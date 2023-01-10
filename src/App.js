9ff17f4e3cd7f1a8c6676367fdd76762d8e65e8a
Serjay Parks <75953992+iLOVEthecolorORANGE@users.noreply.github.com>
2023-01-10 12:32:07 -0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import NavBar from './components/NavBar';
 import About from './pages/About';
 import Rates from './pages/Rates';
@@ -11,12 +11,13 @@ import SitterPage from './pages/SitterPage';
 import Postings from './pages/Postings';
 import Edit from './postings/Edit'
 
+
 function App() {
   return (
     <div className="App">
       <NavBar />
         <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/sitters" element={<Sitters/>} />
         <Route path="/about" element={<About />} />
         <Route path="/rates" element={<Rates />} />
@@ -24,6 +25,7 @@ function App() {
         <Route path="/bookus" element={<CreatePosting />} />
         <Route path="/sitter/:index" element={<SitterPage />} />
         <Route path="/postings" element={<Postings />} />
+        <Route path="/*" element={<Navigate to="/home" />} />
         {/* <Route path="/edit/:id" element={<Edit />} /> */}
       </Routes>
     </div>
